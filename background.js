fcfb6f9cef3bcc876cc723311f76eaf7ef52b96c
Murali Krishna <muralikv@thoughtworks.com>
2015-11-20 18:11:59 +0530

    Added style for scroll to top div

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
 var div = document.createElement("div");
-div.innerHtml = "Scrool to top";
+div.innerHTML = "^";
+div.id = "scroll-to-top-extention-div";
+div.title = "Scroll to Top";
+div.setAttribute('style', 'position: fixed;' +
+	'padding: 10px 13px;' +
+    'background: #464545;' +
+    'z-index: 10000000;' +
+    'right: 10px;' +
+    'bottom: 10px;' +
+    'border-radius: 3px;' +
+    'color: white;' + 
+    'cursor: pointer;' +
+    'font-weight: bold'
+    );
 document.getElementsByTagName("body")[0].appendChild(div);
